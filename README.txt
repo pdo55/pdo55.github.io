635d6e0cab287d53a5615b2edb0056a338d68735
pdo55 <pdo55@g.ucla.edu>
2020-08-19 01:31:18 -0700

    finish layout

--- README.txt
+++
@@ -1,3 +0,0 @@
-Hi there!
-
-This is git repo for my portfolio. Please head over to 
---
+++ js/projects.js
@@ -0,0 +1,87 @@
+var projectShootingAI =
+    '<div class="project p-light" id="p-shooting-ai">' +
+    '<a class="overlay" href="https://github.com/pdo55/reinforcement-learning-game" target="_blank">' +
+    '<div class="content vertical-center">' +
+    '<h4>Developer</h4>' +
+    '<h1>Shooting AI</h1>' +
+    '<p>Unbeatable shooting agents trained with q-learning algorithm</p>' +
+    '<button>Explore <span class="arrow"></span></button>' +
+    '</div>' +
+    '</a>' +
+    '</div>';
+
+// var eMotionSmartJournal =
+//     '<div class="project p-dark" id="p-e-motion-smart-journal">' +
+//     '<a class="overlay" href="project/e-motion-smart-journal.html" target="_blank">' +
+//     '<div class="content vertical-center">' +
+//     '<h4>Developer</h4>' +
+//     '<h1>E-motion Smart Journal</h1>' +
+//     '<p>Mental health platform powered by journaling and machine learning.</p>' +
+//     '<button>Explore <span class="arrow"></span></button>' +
+//     '</div>' +
+//     '</a>' +
+//     '</div>';
+
+
+
+var featured = [
+    projectShootingAI,
+];
+
+// var all = [
+//     projectOptimo,
+//     projectArticleSummaryDeepLearning,
+//     projectYelpReviewClassifier,
+//     projectAiChatbot,
+//     projectColourGame,
+//     RecursiveTrees,
+//     projectBeautifulTodo,
+//     projectBeatVisualizer,
+//     projectIrisClassifier,
+//     projectApexTowerDefense,
+//     projectSpaceInvaders,
+//     projectAsteroids,
+//     projectBubblesortDemonstration,
+//     projectPixelShooter,
+//     ColourMatch,
+//     SortingComparison,
+// ];
+
+// var frontend = [
+//     projectZipcall,
+//     eMotionSmartJournal,
+//     projectArticleSummaryDeepLearning,
+//     projectOptimo,
+//     projectStockNewsAnalysis,
+//     projectBeautifulTodo,
+//     projectTickeriQ,
+//     projectColourGame,
+//     projectBeatVisualizer,
+//     RecursiveTrees,
+//     ColourMatch,
+// ];
+
+// var datascience = [
+//     projectZipcall,
+//     projectStockNewsAnalysis,
+//     eMotionSmartJournal,
+//     projectArticleSummaryDeepLearning,
+//     projectTickeriQ,
+//     projectYelpReviewClassifier,
+//     projectAiChatbot,
+//     projectIrisClassifier,
+//     RecursiveTrees,
+//     SortingComparison,
+//     projectBubblesortDemonstration,
+
+// ];
+
+// var game = [
+//     projectColourGame,
+//     projectApexTowerDefense,
+//     projectBeatVisualizer,
+//     ColourMatch,
+//     projectSpaceInvaders,
+//     projectAsteroids,
+//     projectPixelShooter
+// ];
